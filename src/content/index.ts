4751de9a2611afc0e5120c51cfe6793e56daf228
Newton Kumar <newtonmauryadw@gmail.com>
2022-02-07 18:32:55 +0530

    Update index.ts

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,18 @@
-console.log('content script')
+window.addEventListener('click', () => {
+
+    let getClick = localStorage.getItem("isClicked")
+
+    console.log("Hello Newton!", "getClicked", getClick)
+    if (getClick === "true") {
+      alert("Clicked on and it's true")
+    }
+    else if(getClick === "false") {
+        alert("Stop click on and getClick is false")
+    }
+    chrome.runtime.sendMessage(
+      'Take screenshot',
+      (responce) => {
+        console.log("responce", responce)
+      },
+    )
+  })
