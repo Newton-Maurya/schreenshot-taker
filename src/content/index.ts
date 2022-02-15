5d9b6742f0cd305faaed193f476e1c4377a67442
Newton Kumar <newtonmauryadw@gmail.com>
2022-02-15 19:47:48 +0530

    Update index.ts

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,18 +1,40 @@
-window.addEventListener('click', () => {
 
-    let getClick = localStorage.getItem("isClicked")
 
-    console.log("Hello Newton!", "getClicked", getClick)
-    if (getClick === "true") {
-      alert("Clicked on and it's true")
-    }
-    else if(getClick === "false") {
-        alert("Stop click on and getClick is false")
-    }
-    chrome.runtime.sendMessage(
-      'Take screenshot',
-      (responce) => {
-        console.log("responce", responce)
-      },
-    )
+
+window.addEventListener('load', () => {  
+  chrome.storage.sync.set({ isClicked: false }, () => {})
+  chrome.storage.sync.set({lastImage: ""}, () => {})
+})
+
+let count = 0;
+const screenshotTaker = (event: any) => {
+  console.log("Inside screenTaker, event.clientX, event.clientY", event.clientX, event.clientY)
+  chrome.storage.sync.get('isClicked', (data) =>{
+    let condition = data ? data.isClicked: false
+    if(condition) {
+      count++
+      chrome.runtime.sendMessage({name: "Record Click", count: count, Xcoordinate: event.clientX, Ycoordinate: event.clientY})
+      }
+    })
+  }
+
+window.addEventListener('click', (event) => {
+  screenshotTaker(event)
+})
+
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+
+  chrome.runtime.sendMessage({name: "screenshot"}, (res) => {
+
+
+    // chrome.storage.sync.set({imageString: res}, () => {})
+    // console.log("Screenshot taken", res, request)
+    // Promise.resolve("").then(result => sendResponse(result));
+    // return true;
   })
+  sendResponse("responce sent")
+});
+
+// let vr = 
+
+// console.log('vr', vr)
